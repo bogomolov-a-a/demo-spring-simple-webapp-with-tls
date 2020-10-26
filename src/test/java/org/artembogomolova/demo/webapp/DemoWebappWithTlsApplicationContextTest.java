3c15620ae19242c87928c4ebfec8426d1e6a624a
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-10-26 18:35:21 +0300

    all code quality checks added(static analysis,test)

--- src/test/java/org/artembogomolova/demo/webapp/DemoWebappWithTlsApplicationContextTest.java
+++ src/test/java/org/artembogomolova/demo/webapp/DemoWebappWithTlsApplicationContextTest.java
@@ -1,6 +1,5 @@
 package org.artembogomolova.demo.webapp;
 
-import org.artembogomolova.demo.webapp.DemoWebappWithTlsApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +10,7 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = {DemoWebappWithTlsApplication.class})
 @AutoConfigureMockMvc
 @ActiveProfiles(value = {"test"})
-public class DemoWebappWithTlsApplicationTests {
+public class DemoWebappWithTlsApplicationContextTest {
 
   @LocalServerPort
   private int serverPort;
