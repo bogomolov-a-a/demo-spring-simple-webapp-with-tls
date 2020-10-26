c80c8da1cf5c4f48074ee07906041736554879b8
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-10-27 02:28:36 +0300

    remove unused method implementations

--- src/main/java/org/artembogomolova/demo/webapp/dao/util/SQLLiteDialect.java
+++ src/main/java/org/artembogomolova/demo/webapp/dao/util/SQLLiteDialect.java
@@ -1,13 +1,10 @@
 package org.artembogomolova.demo.webapp.dao.util;
 
 import java.sql.Types;
-import org.hibernate.MappingException;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.function.SQLFunctionTemplate;
 import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.dialect.function.VarArgsSQLFunction;
-import org.hibernate.dialect.identity.IdentityColumnSupport;
-import org.hibernate.dialect.identity.IdentityColumnSupportImpl;
 import org.hibernate.type.StringType;
 
 /**
@@ -59,118 +56,9 @@ public class SQLLiteDialect extends Dialect {
     };
   }
 
-  public boolean supportsTemporaryTables() {
-    return true;
-  }
-
-  public String getCreateTemporaryTableString() {
-    return "create temporary table if not exists";
-  }
-
-  public boolean dropTemporaryTableAfterUse() {
-    return false;
-  }
-
-  @Override
-  public boolean supportsCurrentTimestampSelection() {
-    return true;
-  }
-
-  @Override
-  public boolean isCurrentTimestampSelectStringCallable() {
-    return false;
-  }
-
-  @Override
-  public String getCurrentTimestampSelectString() {
-    return "select current_timestamp";
-  }
-
-  @Override
-  public boolean supportsUnionAll() {
-    return true;
-  }
-
-  @Override
-  public boolean hasAlterTable() {
-    return false; // As specify in NHibernate dialect
-  }
-
-  @Override
-  public boolean dropConstraints() {
-    return false;
-  }
-
-  @Override
-  public String getAddColumnString() {
-    return "add column";
-  }
-
   @Override
   public String getForUpdateString() {
     return "";
   }
 
-  @Override
-  public boolean supportsOuterJoinForUpdate() {
-    return false;
-  }
-
-  @Override
-  public String getDropForeignKeyString() {
-    throw new UnsupportedOperationException("No drop foreign key syntax supported by SQLiteDialect");
-  }
-
-  @Override
-  public String getAddForeignKeyConstraintString(String constraintName, String[] foreignKey, String referencedTable,
-      String[] primaryKey, boolean referencesPrimaryKey) {
-    throw new UnsupportedOperationException("No add foreign key syntax supported by SQLiteDialect");
-  }
-
-  @Override
-  public String getAddPrimaryKeyConstraintString(String constraintName) {
-    throw new UnsupportedOperationException("No add primary key syntax supported by SQLiteDialect");
-  }
-
-  @Override
-  public boolean supportsIfExistsBeforeTableName() {
-    return true;
-  }
-
-  @Override
-  public boolean supportsCascadeDelete() {
-    return false;
-  }
-
-  @Override
-  public IdentityColumnSupport getIdentityColumnSupport() {
-    return new IdentityColumnSupportImpl()
-    {
-      @Override
-      public boolean supportsIdentityColumns() {
-        return true;
-      }
-
-      @Override
-      public String getIdentitySelectString(String table, String column, int type)
-          throws MappingException {
-        return "select last_insert_rowid()";
-      }
-
-      @Override
-      public String getIdentityColumnString(int type) throws MappingException {
-        switch(type)
-        {
-          case Types.BIGINT:
-          {
-            return "bigint";
-          }
-          case Types.INTEGER: {
-            return "integer primary key autoincrement";
-          }
-        }
-        return "integer";
-      }
-    };
-  }
 }
