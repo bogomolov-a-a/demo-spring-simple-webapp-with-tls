ad46ce30aa3d154a7fa827e75c06373192122bc5
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-10-31 19:24:44 +0300

    test certificates generation script fixed.

--- certificate/cert-build.sh
+++ certificate/cert-build.sh
@@ -1,8 +1,14 @@
+echo 'start generating TEST certificates! DON''T USE THESE CERTIFICATE IN PRODUCTION ENVIRONMENT!!!!'
+if [ -z "$JAVA_HOME" ]; then
+  echo 'generating impossible, because variable JAVA_HOME not set. Please set it, and try again!'
+  exit 1
+fi
 cd ./certificate
 rm -rf build
 mkdir build
-./ca-cert-build.sh build 4096 10000  localhost ./conf trust-storage rootCA
+./ca-cert-build.sh build 4096 10000 localhost ./conf trust-storage rootCA
 ./localhost-cert-build.sh build 4096 500 localhost ./conf server-key
 #./application-cert-build.sh build 4096 500 localhost ./conf
 rm build/rootCA.key
 rm build/rootCA.crt
+echo 'TEST certificates build!'
--- certificate/localhost-cert-build.sh
+++ certificate/localhost-cert-build.sh
@@ -1,9 +1,11 @@
-openssl genrsa -out $1/localhost.key $2
-openssl req -new -subj "/CN=localhost/ST=St.Petersburg/C=RU/O=ZORN LEMMA/OU=develop" -config $5/openssl-server.conf -reqexts server_cert -key $1/localhost.key -out $1/localhost.csr
-openssl x509 -req -extfile $5/openssl-server.conf -extensions server_cert -CAcreateserial -days $3 -CA $1/rootCA.crt -CAkey $1/rootCA.key -in $1/localhost.csr -out $1/localhost.crt
-rm $1/localhost.csr
-openssl pkcs12 -export -out $1/$6.p12 -inkey $1/localhost.key -in $1/localhost.crt -certfile $1/localhost.crt -certfile $1/rootCA.crt -name $4 -password \
-pass:$4 -passout pass:$4
-keytool -importcert -noprompt -keystore $1/$6.p12 -storepass $4 -alias $6 -file $1/rootCA.crt
-rm $1/localhost.key
-rm $1/localhost.crt
+echo 'start TLS key pair generating'
+openssl genrsa -out "$1"/localhost.key "$2"
+openssl req -new -config "$5"/openssl-server.conf -reqexts server_cert -subj "/CN=localhost/ST=St.Petersburg/C=RU/O=ZORN LEMMA/OU=develop" -key "$1"/localhost.key -out "$1"/localhost.csr
+openssl x509 -req -extfile "$5"/openssl-server.conf -extensions server_cert -CAcreateserial -days "$3" -CA "$1"/rootCA.crt -CAkey "$1"/rootCA.key -in "$1"/localhost.csr -out "$1"/localhost.crt
+rm "$1"/localhost.csr
+openssl pkcs12 -export -out "$1"/"$6".p12 -inkey "$1"/localhost.key -in "$1"/localhost.crt -certfile "$1"/localhost.crt -certfile "$1"/rootCA.crt -name "$4" -password \
+  pass:"$4" -passout pass:"$4"
+keytool -importcert -noprompt -keystore "$1"/"$6".p12 -storepass "$4" -alias "$6" -file "$1"/rootCA.crt
+rm "$1"/localhost.key
+rm "$1"/localhost.crt
+echo 'TLS key pair generated!'
