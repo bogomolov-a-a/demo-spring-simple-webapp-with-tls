054e0894d1a2cb7595a33612fe102424f16b673c
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-11-02 20:29:30 +0300

    fixed sonar workflow

--- CI/issue-management.sh
+++ CI/issue-management.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 function createIssue() {
-  body=$(echo -e 'Cause:'"$(echo $1 | jq -cr '.message')\n'Rule:'$(echo $1 | jq -cr '.rule')"\n);
+  body=$(echo -e 'Cause:'"$(echo $1 | jq -cr '.message')\n'Rule:'$(echo $1 | jq -cr '.rule')"\n)
   issueData='{"title":"Fix:'$(echo -e "$(echo $1 | jq -cr '.message')")'","body":"'$body'"''}'
   curl \
     -H "Accept: application/vnd.github.v3+json" \
-    ${{github.api_url}}/repos/${{ github.repoisitory }}/issues \
+    "$GITHUB_REPO_ISSUE_URL" \
     -d $issueData
-    
+
 }
-sonarIssueList=$(curl https://sonarcloud.io/api/issues/search?projectKeys=bogomolov-a-a_demo-spring-simple-webapp-with-tls |
+sonarIssueList=$(curl "$SONAR_QUBE_PROJECT_ISSUE_URL" |
   jq -c '.issues[]' | jq -c '{message,pullRequest,rule, component,line,textRange}')
 while IFS="}}" read -ra ADDR; do
   for i in "${ADDR[@]}"; do
