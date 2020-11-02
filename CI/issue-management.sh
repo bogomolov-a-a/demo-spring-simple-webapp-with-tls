1532c5292f65472da9617980a638bd315b2d473d
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-11-02 20:57:01 +0300

    fixed sonar workflow,3

--- CI/issue-management.sh
+++ CI/issue-management.sh
@@ -2,15 +2,28 @@
 function createIssue() {
   body=$(echo -e 'Cause:'"$(echo $1 | jq -cr '.message')\n'Rule:'$(echo $1 | jq -cr '.rule')"\n)
   issueData='{"title":"Fix:'$(echo -e "$(echo $1 | jq -cr '.message')")'","body":"'$body'"''}'
-  curl -H 'Accept: application/vnd.github.v3+json' $(echo -e"$GITHUB_REPO_ISSUE_URL") -d $issueData
+  githubIssuesCommand='curl -H ''Accept: application/vnd.github.v3+json'' $(echo -e"$GITHUB_REPO_ISSUE_URL") -d $issueData'
+  echo 'executing "'$githubIssuesCommand'"'
+  $githubIssuesCommand
+  if [[ $? -ne 0 ]]; then
+    exit 1
+  fi
+  echo '"'$githubIssuesCommand'" success executed'
 }
-curlCommand='curl'$(echo -e "$SONAR_QUBE_PROJECT_ISSUE_URL");
-echo 'executing '$curlCommand
-sonarIssueList=$( $curlCommand | jq -c '.issues[]' | jq -c '{message,pullRequest,rule, component,line,textRange}')
+sonarCommand='curl'$(echo -e "$SONAR_QUBE_PROJECT_ISSUE_URL")
+echo 'executing "'$sonarCommand'"'
+sonarIssueList=$($sonarCommand | jq -c '.issues[]' | jq -c '{message,pullRequest,rule, component,line,textRange}')
+if [[ $? -ne 0 ]]; then
+  exit 1
+fi
 while IFS="}}" read -ra ADDR; do
   for i in "${ADDR[@]}"; do
     if [[ -n $i ]]; then
       createIssue "$i"'}}'
+      if [[ $? -ne 0 ]]; then
+        exit 1
+      fi
     fi
   done
 done <<<"$sonarIssueList"
+echo '"'$sonarCommand'" success executed'
