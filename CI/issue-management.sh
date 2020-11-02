6a06a415b4d75540b214811534f4ea727bdda1a0
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-11-02 21:57:03 +0300

    fixed github issue creating command,2. issue data

--- CI/issue-management.sh
+++ CI/issue-management.sh
@@ -2,7 +2,7 @@
 function createIssue() {
   body=$(echo -e 'Cause:'"$(echo $1 | jq -cr '.message')\n'Rule:'$(echo $1 | jq -cr '.rule')"\n)
   issueData='{"title":"Fix:'$(echo -e "$(echo $1 | jq -cr '.message')")'","body":"'$body'"''}'
-  githubIssuesCommand='curl -H ''Accept: application/vnd.github.v3+json''' $(echo -e "$GITHUB_REPO_ISSUE_URL")' -d $issueData'
+  githubIssuesCommand='curl -H ''Accept: application/vnd.github.v3+json''' $(echo -e "$GITHUB_REPO_ISSUE_URL")' -d '$issueData
   echo 'executing "'$githubIssuesCommand'"'
   $githubIssuesCommand
   if [[ $? -ne 0 ]]; then
