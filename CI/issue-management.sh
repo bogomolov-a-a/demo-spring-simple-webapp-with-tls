7a5480d5ef4f799b2eb22159b008bf47fbe002e4
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-11-02 20:42:43 +0300

    fixed sonar workflow, remove new line 2, echo -e added

--- CI/issue-management.sh
+++ CI/issue-management.sh
@@ -2,10 +2,9 @@
 function createIssue() {
   body=$(echo -e 'Cause:'"$(echo $1 | jq -cr '.message')\n'Rule:'$(echo $1 | jq -cr '.rule')"\n)
   issueData='{"title":"Fix:'$(echo -e "$(echo $1 | jq -cr '.message')")'","body":"'$body'"''}'
-  curl \
-    -H "Accept: application/vnd.github.v3+json" "$GITHUB_REPO_ISSUE_URL" -d $issueData
+  curl -H 'Accept: application/vnd.github.v3+json' $(echo -e"$GITHUB_REPO_ISSUE_URL") -d $issueData
 }
-sonarIssueList=$(curl "$SONAR_QUBE_PROJECT_ISSUE_URL" | jq -c '.issues[]' | jq -c '{message,pullRequest,rule, component,line,textRange}')
+sonarIssueList=$(curl $(echo -e "$SONAR_QUBE_PROJECT_ISSUE_URL") | jq -c '.issues[]' | jq -c '{message,pullRequest,rule, component,line,textRange}')
 while IFS="}}" read -ra ADDR; do
   for i in "${ADDR[@]}"; do
     if [[ -n $i ]]; then
