6ec8b537135e1014dbdc622946a17c9388b8221b
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-11-03 22:47:12 +0300

    rewrite issue-management.sh with http code control

--- CI/issue-management.sh
+++ CI/issue-management.sh
@@ -1,36 +1,58 @@
 #!/bin/bash
+
+exchangeFileName=./output.json
+
+function sendSonarRequest() {
+  sonarResponseGood=200
+  #$(echo -e "$SONAR_QUBE_PROJECT_ISSUE_URL"){
+  echo sending sonar request
+  code=$(curl -s -o $exchangeFileName -w '%{http_code}' https://sonarcloud.io/api/issues/search?projectKeys=bogomolov-a-a_demo-spring-simple-webapp-with-tls)
+  if [[ "$code" -ne "$sonarResponseGood" ]]; then
+    echo Code "$code" don\'t valid code for sonar response. Excepted "$sonarResponseGood"
+    return 1 # exit from function with error
+  fi
+  echo sonar request success sent
+}
+
 function createIssue() {
+  exceptedCode=201
   body=$(echo -e "Cause_$(echo $1 | jq -cr '.message')Rule $(echo $1 | jq -cr '.message')" | tr ' ' '_' | tr ''\''' '_' | tr ''\"'' '_' | tr ':' '_')
-  #, )
   title=$(echo -e $(echo $1 | jq -cr '.message') | tr ' ' '_' | tr ''\''' '_' | tr ''\"'' '_' | tr ':' '_')
   issueData='{"title":"Fix_'$title'","body":"'$body'"}'
-  githubIssuesCommand="curl -X POST -d '"$issueData"' -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/bogomolov-a-a/demo-spring-simple-webapp-with-tls/issues"
-  # $(echo -e "$GITHUB_REPO_ISSUE_URL")"
+  # https://api.github.com/repos/bogomolov-a-a/demo-spring-simple-webapp-with-tls/issues
   echo 'executing "'$githubIssuesCommand'"'
-  $githubIssuesCommand
-  if [[ $? -ne 0 ]]; then
-    exit 1
+  code=$(curl -X -v -o $exchangeFileName -d '"$issueData"' -H 'Accept: application/vnd.github.v3+json' $(echo -e "$GITHUB_REPO_ISSUE_URL"))
+  if [[ "$code" -ne "$exceptedCode" ]]; then
+    echo Code "$code" don\'t valid code for github issue response. Excepted "$sonarResponseGood"
+    return 1
   fi
   echo '"'$githubIssuesCommand'" success executed'
 }
-sonarCommand='curl  https://sonarcloud.io/api/issues/search?projectKeys=bogomolov-a-a_demo-spring-simple-webapp-with-tls' #$(echo -e "$SONAR_QUBE_PROJECT_ISSUE_URL")
-echo 'executing "'$sonarCommand'"'
-$sonarCommand >./output.json
-if [[ $? -ne 0 ]]; then
-  exit 1
-fi
-sonarIssueList=$(cat ./output.json | jq -c '.issues[]' | jq -c '{message,pullRequest,rule, component,line,textRange}')
-if [[ $? -ne 0 ]]; then
-  exit 1
-fi
-while IFS="}}" read -ra issues; do
-  for i in "${issues[@]}"; do
-    if [[ -n $i ]]; then
-      createIssue "$i"'}}'
-      if [[ $? -ne 0 ]]; then
-        exit 1
+
+function handleSonarResponse() {
+  sonarIssueList=$(cat "$exchangeFileName" | jq -c '.issues[]' | jq -c '{message,pullRequest,rule, component,line,textRange}')
+  if [[ $? -ne 0 ]]; then
+    return 1
+  fi
+  while IFS="}}" read -ra issues; do
+    for i in "${issues[@]}"; do
+      if [[ -n $i ]]; then
+        createIssue "$i"'}}'
+        if [[ $? -ne 0 ]]; then
+          return 1
+        fi
       fi
-    fi
-  done
-done <<<"$sonarIssueList"
-echo '"'$sonarCommand'" success executed'
+    done
+  done <<<"$sonarIssueList"
+}
+sendSonarRequest
+if [[ $? -ne 0 ]]; then
+  rm $exchangeFileName
+  exit $?
+fi
+handleSonarResponse
+if [[ $? -ne 0 ]]; then
+  rm $exchangeFileName
+  exit $?
+fi
+rm $exchangeFileName
