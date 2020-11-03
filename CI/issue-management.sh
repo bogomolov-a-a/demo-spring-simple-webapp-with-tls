827810d08973424581b423dcbccc48adb0341db3
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-11-03 23:46:21 +0300

    try to finalk commmit issue integration

--- CI/issue-management.sh
+++ CI/issue-management.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-exchangeFileName=./output.json
-
+exchangeFileName=./output
+# send request to sonar qube server, get issue list from it
+# and save result into $exchangeFileName file path.
+# use curl http_code for control success of the sending.
+# if code != 200 (OK) then script fail.
 function sendSonarRequest() {
   sonarResponseGood=200
   echo sending sonar request
@@ -12,21 +15,27 @@ function sendSonarRequest() {
   fi
   echo sonar request success sent
 }
-
-function createIssue() {
-  exceptedCode=201
-  body=$(echo -e "Cause_$(echo $1 | jq -cr '.message')Rule $(echo $1 | jq -cr '.message')" | tr ' ' '_' | tr ''\''' '_' | tr ''\"'' '_' | tr ':' '_')
+#Create issue from sonar qube issue data.
+#Send  POST request to issues api url
+#format {"title": "FIX_"+essence_of_the_task, "body":essence_of_the_task+"Rule"+ violation_rule_name+....}
+function createGithubIssue() {
+  gitHubExceptedCode=201
+  echo creating github issue
   title=$(echo -e $(echo $1 | jq -cr '.message') | tr ' ' '_' | tr ''\''' '_' | tr ''\"'' '_' | tr ':' '_')
-  issueData='{"title":"Fix_'$title'","body":"'$body'"}'
-  # https://api.github.com/repos/bogomolov-a-a/demo-spring-simple-webapp-with-tls/issues
-  code=$(curl -s -X -o $exchangeFileName -w '%{http_code}' -d '"$issueData"' -H 'Accept: application/vnd.github.v3+json' $(echo -e "$GITHUB_REPO_ISSUE_URL"))
-  if [[ "$code" -ne "$exceptedCode" ]]; then
-    echo Code "$code" don\'t valid code for github issue response. Excepted "$sonarResponseGood"
+  echo with title $title
+  body=$(echo -e "Cause_$(echo $1 | jq -cr '.message')Rule $(echo $1 | jq -cr '.message')" |
+    tr ' ' '_' | tr ''\''' '_' | tr ''\"'' '_' | tr ':' '_')
+  echo with body $body
+  code=$(curl -s -X -o $exchangeFileName -w '%{http_code}' -d '{"title":"Fix_'$title'","body":"'$body'"}' \
+    -H 'Authorization: token '$ISSUES_TOKEN -H 'Accept: application/vnd.github.v3+json' $(echo -e "$GITHUB_REPO_ISSUE_URL"))
+  if [[ "$code" -ne "$gitHubExceptedCode" ]]; then
+    echo Code "$code" don\'t valid code for github issue response. Excepted "$gitHubExceptedCode"
+    cat $exchangeFileName
     return 1
   fi
-  echo '"'$githubIssuesCommand'" success executed'
+  echo github issue success created
 }
-
+# for each sonar issue create github issue
 function handleSonarResponse() {
   sonarIssueList=$(cat "$exchangeFileName" | jq -c '.issues[]' | jq -c '{message,pullRequest,rule, component,line,textRange}')
   if [[ $? -ne 0 ]]; then
@@ -35,14 +44,15 @@ function handleSonarResponse() {
   while IFS="}}" read -ra issues; do
     for i in "${issues[@]}"; do
       if [[ -n $i ]]; then
-        createIssue "$i"'}}'
+        createGithubIssue "$i"'}}'
         if [[ $? -ne 0 ]]; then
-          return 1
+          exit 1
         fi
       fi
     done
   done <<<"$sonarIssueList"
 }
+# main script
 sendSonarRequest
 if [[ $? -ne 0 ]]; then
   rm $exchangeFileName
