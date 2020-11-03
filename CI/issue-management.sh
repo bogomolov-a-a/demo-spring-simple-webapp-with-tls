0fbe052a1e8cad86f7c59f13693d57faadd24a2d
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-11-03 22:52:52 +0300

    fixed github issue create request http code handling

--- CI/issue-management.sh
+++ CI/issue-management.sh
@@ -4,9 +4,8 @@ exchangeFileName=./output.json
 
 function sendSonarRequest() {
   sonarResponseGood=200
-  #$(echo -e "$SONAR_QUBE_PROJECT_ISSUE_URL"){
   echo sending sonar request
-  code=$(curl -s -o $exchangeFileName -w '%{http_code}' https://sonarcloud.io/api/issues/search?projectKeys=bogomolov-a-a_demo-spring-simple-webapp-with-tls)
+  code=$(curl -s -o $exchangeFileName -w '%{http_code}' $(echo -e "$SONAR_QUBE_PROJECT_ISSUE_URL"))
   if [[ "$code" -ne "$sonarResponseGood" ]]; then
     echo Code "$code" don\'t valid code for sonar response. Excepted "$sonarResponseGood"
     return 1 # exit from function with error
@@ -20,8 +19,7 @@ function createIssue() {
   title=$(echo -e $(echo $1 | jq -cr '.message') | tr ' ' '_' | tr ''\''' '_' | tr ''\"'' '_' | tr ':' '_')
   issueData='{"title":"Fix_'$title'","body":"'$body'"}'
   # https://api.github.com/repos/bogomolov-a-a/demo-spring-simple-webapp-with-tls/issues
-  echo 'executing "'$githubIssuesCommand'"'
-  code=$(curl -X -v -o $exchangeFileName -d '"$issueData"' -H 'Accept: application/vnd.github.v3+json' $(echo -e "$GITHUB_REPO_ISSUE_URL"))
+  code=$(curl -s -X -o $exchangeFileName -w '%{http_code}' -d '"$issueData"' -H 'Accept: application/vnd.github.v3+json' $(echo -e "$GITHUB_REPO_ISSUE_URL"))
   if [[ "$code" -ne "$exceptedCode" ]]; then
     echo Code "$code" don\'t valid code for github issue response. Excepted "$sonarResponseGood"
     return 1
