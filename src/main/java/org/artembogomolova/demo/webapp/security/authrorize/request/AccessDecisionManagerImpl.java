c57155cd95b9fd665a48125f8dd1d13991c35ce6
Artem Bogomolov <42538726+bogomolov-a-a@users.noreply.github.com>
2020-11-14 13:50:03 +0300

    Closes #107, fixed. (#126)

---
+++ src/main/java/org/artembogomolova/demo/webapp/security/authrorize/request/AccessDecisionManagerImpl.java
@@ -0,0 +1,30 @@
+package org.artembogomolova.demo.webapp.security.authrorize.request;
+
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class AccessDecisionManagerImpl implements AccessDecisionManager {
+    @Override
+    public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes)
+            throws AccessDeniedException, InsufficientAuthenticationException {
+
+    }
+
+    @Override
+    public boolean supports(ConfigAttribute attribute) {
+        return true;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.equals(FilterInvocation.class);
+    }
+}
