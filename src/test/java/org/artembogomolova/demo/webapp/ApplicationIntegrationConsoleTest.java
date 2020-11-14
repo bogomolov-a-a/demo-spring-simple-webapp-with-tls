0312212fd3f4affd06dff535b6390b583635f7b5
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-11-15 00:45:30 +0300

    #129, added console test for application.

---
+++ src/test/java/org/artembogomolova/demo/webapp/ApplicationIntegrationConsoleTest.java
@@ -0,0 +1,12 @@
+package org.artembogomolova.demo.webapp;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationIntegrationConsoleTest {
+
+  @Test
+  public void applicationRun() {
+    DemoWebappWithTlsApplication.main(new String[]{});
+  }
+
+}
