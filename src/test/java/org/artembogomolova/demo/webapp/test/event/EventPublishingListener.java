e62f1b00f9474b8495e92fb9a2b80f4f9a37790b
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-11-22 00:06:05 +0300

    #141, added test event listener

---
+++ src/test/java/org/artembogomolova/demo/webapp/test/event/EventPublishingListener.java
@@ -0,0 +1,31 @@
+package org.artembogomolova.demo.webapp.test.event;
+
+import org.artembogomolova.demo.webapp.dao.repo.IAuthorityRepository;
+import org.artembogomolova.demo.webapp.dao.repo.IUserRepository;
+import org.artembogomolova.demo.webapp.dao.repo.IUserRoleRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.event.EventPublishingTestExecutionListener;
+
+public class EventPublishingListener extends EventPublishingTestExecutionListener {
+
+  @Override
+  public void beforeTestMethod(TestContext testContext) {
+    super.beforeTestExecution(testContext);
+    ApplicationContext applicationContext = testContext.getApplicationContext();
+    applicationContext.publishEvent(new ContextStartedEvent(applicationContext));
+  }
+
+  @Override
+  public void afterTestMethod(TestContext testContext) {
+    ConfigurableApplicationContext applicationContext = (ConfigurableApplicationContext) testContext.getApplicationContext();
+    IUserRepository userRepository = applicationContext.getBeanFactory().getBean(IUserRepository.class);
+    IUserRoleRepository userRoleRepository = applicationContext.getBeanFactory().getBean(IUserRoleRepository.class);
+    IAuthorityRepository authorityRepository = applicationContext.getBeanFactory().getBean(IAuthorityRepository.class);
+    userRepository.deleteAll();
+    userRoleRepository.deleteAll();
+    authorityRepository.deleteAll();
+  }
+}
