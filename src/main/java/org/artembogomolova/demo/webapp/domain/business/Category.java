6f6063d09f4bd0926ac0af4dd1748492e0d861e0
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-11-28 23:23:42 +0300

    #144, copy constructor added, relationship fixed.

--- src/main/java/org/artembogomolova/demo/webapp/domain/business/Category.java
+++ src/main/java/org/artembogomolova/demo/webapp/domain/business/Category.java
@@ -5,13 +5,17 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.artembogomolova.demo.webapp.dao.repo.business.ICategoryRepository;
 import org.artembogomolova.demo.webapp.domain.core.IdentifiedEntity;
+import org.artembogomolova.demo.webapp.validation.UniqueMultiColumnConstraint;
+import org.artembogomolova.demo.webapp.validation.UniqueMultiColumnConstraint.UniqueMultiColumnConstraintColumns;
 
 @Entity
 @Table(name = "categories")
@@ -19,16 +23,31 @@ import org.artembogomolova.demo.webapp.domain.core.IdentifiedEntity;
 @Getter
 @ToString(exclude = {"goods", "actions"})
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@UniqueMultiColumnConstraint(repository = ICategoryRepository.class,
+    constraints = @UniqueMultiColumnConstraintColumns(name = Category.BASIC_CONSTRAINT_NAME,
+        value = {Category.NAME_FIELD_NAME, Category.PARENT_CATEGORY_ID_NAME}))
 public class Category extends IdentifiedEntity {
 
+  public static final String BASIC_CONSTRAINT_NAME = "basicConstraint";
+  public static final String NAME_FIELD_NAME = "name";
+  public static final String PARENT_CATEGORY_ID_NAME = "parentCategoryId";
   @Setter
   @EqualsAndHashCode.Include
   private String name;
   @Setter
   @EqualsAndHashCode.Include
   private Long parentCategoryId;
-  @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE, CascadeType.DETACH}, orphanRemoval = true, mappedBy = "producer")
+  @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE, CascadeType.DETACH}, orphanRemoval = true, mappedBy = "category")
   private List<Good> goods = new ArrayList<>();
-  @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE, CascadeType.DETACH}, orphanRemoval = true, mappedBy = "good")
-  private List<Action> actions = new ArrayList<>();
+  @OneToOne(mappedBy = "category", cascade = {CascadeType.MERGE, CascadeType.REFRESH})
+  @Setter
+  private Action action;
+
+  public Category(Category category) {
+    super(category);
+    this.setName(category.getName());
+    this.setParentCategoryId(category.getParentCategoryId());
+    this.getGoods().addAll(category.getGoods());
+    this.setAction(category.getAction());
+  }
 }
