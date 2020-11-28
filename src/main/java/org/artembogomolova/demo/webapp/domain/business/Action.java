5d806254f1e12270db7255ee2aa3d592e17cb256
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-11-28 23:23:34 +0300

    #144, copy constructor added, relationship fixed.

--- src/main/java/org/artembogomolova/demo/webapp/domain/business/Action.java
+++ src/main/java/org/artembogomolova/demo/webapp/domain/business/Action.java
@@ -1,19 +1,23 @@
 package org.artembogomolova.demo.webapp.domain.business;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.artembogomolova.demo.webapp.dao.repo.IActionRepository;
+import org.artembogomolova.demo.webapp.dao.repo.business.IActionRepository;
 import org.artembogomolova.demo.webapp.domain.core.IdentifiedEntity;
 import org.artembogomolova.demo.webapp.validation.UniqueMultiColumnConstraint;
 import org.artembogomolova.demo.webapp.validation.UniqueMultiColumnConstraint.UniqueMultiColumnConstraintColumns;
@@ -21,45 +25,63 @@ import org.artembogomolova.demo.webapp.validation.UniqueMultiColumnConstraint.Un
 @Entity
 @Table(name = "actions")
 @Getter
-@Setter
-@ToString(exclude = {"category", "good"})
+@ToString(exclude = {"category", "goods"}, callSuper = true)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
 @UniqueMultiColumnConstraint(repository = IActionRepository.class,
-    constraints = {@UniqueMultiColumnConstraintColumns(name = Action.BASIC_CONSTRAINT,
-        value = {"name", "categoryId", "goodId", "startDate"}
+    constraints = {@UniqueMultiColumnConstraintColumns(name = Action.BASIC_CONSTRAINT_NAME,
+        value = {Action.NAME_FIELD_NAME, Action.START_DATE_FIELD_NAME}
     )}
 )
 public class Action extends IdentifiedEntity {
 
-  public static final String BASIC_CONSTRAINT = "actionUniqueConstraint";
+  public static final String BASIC_CONSTRAINT_NAME = "basicConstraint";
+  public static final String NAME_FIELD_NAME = "name";
+  public static final String START_DATE_FIELD_NAME = "startDate";
+  @Setter
   @EqualsAndHashCode.Include
   private String name;
+  @Setter
   private String description;
   @Temporal(TemporalType.TIMESTAMP)
   @EqualsAndHashCode.Include
   private Date startDate;
   @Temporal(TemporalType.TIMESTAMP)
   private Date endDate;
+  @Setter
   private Float discountFixed;
+  @Setter
   private Float discountPercent;
-  @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE, CascadeType.DETACH})
+  @OneToOne(cascade = {CascadeType.ALL})
   @JoinColumn(name = "category_id", columnDefinition = "bigint")
+  @Setter
   private Category category;
-  @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE, CascadeType.DETACH})
-  @JoinColumn(name = "good_id", columnDefinition = "bigint")
-  private Good good;
-  @EqualsAndHashCode.Include
-  @Column(name = "good_id", columnDefinition = "bigint", insertable = false, updatable = false)
-  private Long goodId;
-  @EqualsAndHashCode.Include
+  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "action")
+  private List<Good> goods = new ArrayList<>();
   @Column(name = "category_id", columnDefinition = "bigint", insertable = false, updatable = false)
+  @Setter
   private Long categoryId;
 
-  public Date getStartDate() {
-    if (startDate == null) {
-      return null;
+  public Action(Action copyingEntity) {
+    super(copyingEntity);
+    this.setName(copyingEntity.getName());
+    this.setCategoryId(copyingEntity.getCategoryId());
+    this.setDescription(copyingEntity.getDescription());
+    this.setDiscountFixed(copyingEntity.getDiscountFixed());
+    this.setDiscountPercent(copyingEntity.getDiscountPercent());
+    this.setStartDate(copyingEntity.getStartDate());
+    this.setEndDate(copyingEntity.getEndDate());
+    if(copyingEntity.getCategory()!=null) {
+      this.setCategory(new Category(copyingEntity.getCategory()));
     }
-    return new Date(startDate.getTime());
+    copyingEntity.getGoods().forEach(good->
+    {
+      this.goods.add(new Good(good));
+    });
+  }
+
+  public Date getStartDate() {
+    return startDate != null ? new Date(startDate.getTime()) : null;
   }
 
   public void setStartDate(Date startDate) {
@@ -67,10 +89,7 @@ public class Action extends IdentifiedEntity {
   }
 
   public Date getEndDate() {
-    if (endDate == null) {
-      return null;
-    }
-    return new Date(endDate.getTime());
+    return endDate != null ? new Date(endDate.getTime()) : null;
   }
 
   public void setEndDate(Date endDate) {
