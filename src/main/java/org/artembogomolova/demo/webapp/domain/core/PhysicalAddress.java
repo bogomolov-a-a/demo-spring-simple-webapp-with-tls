1def099e37b159a9be7cff04033286f16efaf9f9
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-12-13 21:53:52 +0300

    #144, class finished.

--- src/main/java/org/artembogomolova/demo/webapp/domain/core/PhysicalAddress.java
+++ src/main/java/org/artembogomolova/demo/webapp/domain/core/PhysicalAddress.java
@@ -2,29 +2,65 @@ package org.artembogomolova.demo.webapp.domain.core;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import lombok.Data;
+import javax.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.artembogomolova.demo.webapp.dao.repo.core.IPhysicalAddressRepository;
 import org.artembogomolova.demo.webapp.domain.IdentifiedEntity;
 import org.artembogomolova.demo.webapp.validation.UniqueMultiColumn;
 import org.artembogomolova.demo.webapp.validation.UniqueMultiColumn.UniqueMultiColumnConstraint;
 
 @Entity
-@Table(name = "addresses")
-@Data
+@Table(name = PhysicalAddress.PHYSICAL_ADDRESSES_TABLE)
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true)
+@EqualsAndHashCode
 @UniqueMultiColumn(repository = IPhysicalAddressRepository.class,
     constraints = {
         @UniqueMultiColumnConstraint(name = IdentifiedEntity.BASIC_CONSTRAINT_NAME,
-            columnNames = {"postalCode", "country", "state", "city", "district", "street", "house", "room", "specificPart"})
+            columnNames = {
+                PhysicalAddress_.POSTAL_CODE,
+                PhysicalAddress_.COUNTRY,
+                PhysicalAddress_.STATE,
+                PhysicalAddress_.CITY,
+                PhysicalAddress_.DISTRICT,
+                PhysicalAddress_.STREET,
+                PhysicalAddress_.HOUSE,
+                PhysicalAddress_.ROOM,
+                PhysicalAddress_.SPECIFIC_PART
+            })
     })
 public class PhysicalAddress extends IdentifiedEntity {
 
+  static final String PHYSICAL_ADDRESSES_TABLE = "physical_addresses";
+  @NotBlank
   private String postalCode;
+  @NotBlank
   private String country;
   private String state;
+  @NotBlank
   private String city;
   private String district;
   private String street;
+  @NotBlank
   private String house;
   private Integer room;
   private String specificPart;
+
+  public PhysicalAddress(PhysicalAddress copyingEntity) {
+    this.setPostalCode(copyingEntity.getPostalCode());
+    this.setCountry(copyingEntity.getCountry());
+    this.setState(copyingEntity.getState());
+    this.setCity(copyingEntity.getCity());
+    this.setDistrict(copyingEntity.getDistrict());
+    this.setStreet(copyingEntity.getStreet());
+    this.setHouse(copyingEntity.getHouse());
+    this.setRoom(copyingEntity.getRoom());
+    this.setSpecificPart(copyingEntity.getSpecificPart());
+  }
 }
