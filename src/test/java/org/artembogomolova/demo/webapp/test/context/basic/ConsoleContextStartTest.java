bb719cc37be67acce9ad92d80e5cf6136f6e5ada
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-12-16 01:13:12 +0300

    #144, fixed inheritance.

--- src/test/java/org/artembogomolova/demo/webapp/test/context/basic/ConsoleContextStartTest.java
+++ src/test/java/org/artembogomolova/demo/webapp/test/context/basic/ConsoleContextStartTest.java
@@ -2,13 +2,19 @@ package org.artembogomolova.demo.webapp.test.context.basic;
 
 import lombok.extern.slf4j.Slf4j;
 import org.artembogomolova.demo.webapp.DemoWebappWithTlsApplication;
+import org.artembogomolova.demo.webapp.test.context.AbstractContextLoadTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @Slf4j
 @DisplayName("Run as console command")
-class ConsoleContextStartTest {
+class ConsoleContextStartTest extends AbstractContextLoadTest {
+
+  ConsoleContextStartTest(@LocalServerPort int serverPort ) {
+    super(serverPort);
+  }
 
   @Test
   @DisplayName("Emulate run as console command without main method execution. Main method body must be duplicate this test")
