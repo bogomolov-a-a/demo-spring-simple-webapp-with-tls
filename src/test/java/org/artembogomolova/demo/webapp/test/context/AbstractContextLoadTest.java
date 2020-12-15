74b0e3e71c65c062ee26791ee01e6bed5a6f1194
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-12-16 01:12:57 +0300

    #144, remove all user after each context test.

--- src/test/java/org/artembogomolova/demo/webapp/test/context/AbstractContextLoadTest.java
+++ src/test/java/org/artembogomolova/demo/webapp/test/context/AbstractContextLoadTest.java
@@ -3,9 +3,12 @@ package org.artembogomolova.demo.webapp.test.context;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.artembogomolova.demo.webapp.DemoWebappWithTlsApplication;
+import org.artembogomolova.demo.webapp.dao.repo.auth.IUserRepository;
 import org.artembogomolova.demo.webapp.test.AbstractTest;
 import org.artembogomolova.demo.webapp.test.util.event.ContextStartedEventPublishingListener;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -22,6 +25,8 @@ public abstract class AbstractContextLoadTest extends AbstractTest {
 
   @Getter
   private int serverPort;
+  @Autowired
+  private IUserRepository userRepository;
 
   protected AbstractContextLoadTest(int serverPort) {
     this.serverPort = serverPort;
@@ -30,5 +35,10 @@ public abstract class AbstractContextLoadTest extends AbstractTest {
   public void checkServerPortAssigned() {
     Assertions.assertNotEquals(0, serverPort, "Port is 0! This is wrong way!");
     log.info("application started at port {}", serverPort);
+  }
+
+  @AfterEach
+  void afterTests() {
+    userRepository.deleteAll();
   }
 }
