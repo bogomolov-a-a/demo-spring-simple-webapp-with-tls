0c9c261bc78d709f755ec28aef494633e02122aa
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2020-12-20 22:01:08 +0300

    #144, good entity test passed!

--- src/main/java/org/artembogomolova/demo/webapp/domain/business/Good.java
+++ src/main/java/org/artembogomolova/demo/webapp/domain/business/Good.java
@@ -10,17 +10,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.artembogomolova.demo.webapp.dao.repo.business.IGoodRepository;
 import org.artembogomolova.demo.webapp.dao.util.SQLite3Dialect;
 import org.artembogomolova.demo.webapp.domain.IdentifiedEntity;
+import org.artembogomolova.demo.webapp.validation.UniqueMultiColumn;
+import org.artembogomolova.demo.webapp.validation.UniqueMultiColumn.UniqueMultiColumnConstraint;
 
 @Entity
 @Table(name = "goods")
 @NoArgsConstructor
 @Getter
-@ToString(exclude = {"producer", "category", "actions", "orderPositions"})
+@ToString(exclude = {Good_.PRODUCER, Good_.CATEGORY})
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@UniqueMultiColumn(repository = IGoodRepository.class, constraints = @UniqueMultiColumnConstraint(name = IdentifiedEntity.BASIC_CONSTRAINT_NAME,
+    columnNames = {Good_.NAME, Good_.PRODUCER, Good_.CATEGORY}))
 public class Good extends IdentifiedEntity {
 
+  @EqualsAndHashCode.Include
   @Setter
   private String name;
   @Setter
@@ -30,15 +36,19 @@ public class Good extends IdentifiedEntity {
   @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE, CascadeType.DETACH})
   @JoinColumn(name = "producer_id", columnDefinition = SQLite3Dialect.FOREIGN_KEY_COLUMN_DEFINITION)
   @Setter
+  @EqualsAndHashCode.Include
   private Producer producer;
   @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE, CascadeType.DETACH})
   @JoinColumn(name = "category_id", columnDefinition = SQLite3Dialect.FOREIGN_KEY_COLUMN_DEFINITION)
   @Setter
+  @EqualsAndHashCode.Include
   private Category category;
 
   public Good(Good good) {
     this.setName(good.getName());
     this.setDescription(good.getDescription());
     this.setImgFilePath(good.getImgFilePath());
+    this.setProducer(good.getProducer() == null ? null : new Producer(good.getProducer()));
+    this.setCategory(good.getCategory() == null ? null : new Category(good.getCategory()));
   }
 }
--- src/test/java/org/artembogomolova/demo/webapp/test/domain/entity/business/GoodEntityTest.java
+++ src/test/java/org/artembogomolova/demo/webapp/test/domain/entity/business/GoodEntityTest.java
@@ -1,12 +1,24 @@
 package org.artembogomolova.demo.webapp.test.domain.entity.business;
 
+import lombok.EqualsAndHashCode;
+import org.artembogomolova.demo.webapp.domain.IdentifiedEntity;
+import org.artembogomolova.demo.webapp.domain.business.Category;
 import org.artembogomolova.demo.webapp.domain.business.Good;
+import org.artembogomolova.demo.webapp.domain.business.Good_;
+import org.artembogomolova.demo.webapp.domain.business.Producer;
 import org.artembogomolova.demo.webapp.test.domain.DomainTestUtil;
 import org.artembogomolova.demo.webapp.test.domain.entity.AbstractAccessorEntityTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 
 @DisplayName("Entity test: Good")
 class GoodEntityTest extends AbstractAccessorEntityTest<Good> {
+
+  private static final String NAME_VALUE = "test good";
+  private static final String DESCRIPTION_VALUE = "test good description";
+  private static final String IMG_FILE_PATH_VALUE = "test file path";
+  private static final Producer PRODUCER_VALUE = new Producer();
+  private static final Category CATEGORY_VALUE = new Category();
 
   GoodEntityTest() {
     super(Good.class,
@@ -17,62 +29,66 @@ class GoodEntityTest extends AbstractAccessorEntityTest<Good> {
   @Override
   protected Good buildStandardEntity() {
     Good result = new Good();
-    updateNameAndDescription(result);
-    // updateQuantityAndPrice(result);
-    updateImageFilePath(result);
-    updateCategory(result);
-    //updateProducer(result);
+    result.setName(NAME_VALUE);
+    result.setDescription(DESCRIPTION_VALUE);
+    result.setImgFilePath(IMG_FILE_PATH_VALUE);
+    result.setProducer(PRODUCER_VALUE);
+    result.setCategory(CATEGORY_VALUE);
     return result;
   }
 
   @Override
+  protected void containFieldCorrectValuesTest(Good standardEntity) {
+    Assertions.assertEquals(NAME_VALUE, standardEntity.getName());
+    Assertions.assertEquals(DESCRIPTION_VALUE, standardEntity.getDescription());
+    Assertions.assertEquals(IMG_FILE_PATH_VALUE, standardEntity.getImgFilePath());
+    Assertions.assertEquals(PRODUCER_VALUE, standardEntity.getProducer());
+    Assertions.assertEquals(CATEGORY_VALUE, standardEntity.getCategory());
+  }
+
+  @Override
   protected Good buildAnotherEntityForTest() {
-    return null;
-  }
-
-  /*private void updateProducer(StockGood result) {
-    // if(result.get)
-  }*/
-
-  private void updateCategory(Good result) {
-    if (result.getCategory() == null) {
-      result.setCategory(DomainTestUtil.buildCategory1());
-    }
-  }
-
-  private void updateImageFilePath(Good result) {
-    if (result.getImgFilePath() == null) {
-      result.setImgFilePath("image.png");
-    }
-
-  }
-
-  /*private void updateQuantityAndPrice(Good result) {
-    if (result.getQuantity() == null) {
-      result.setQuantity(42.0f);
-    }
-    if (result.getPrice() == null) {
-      result.setPrice(4.2f);
-    }
-  }*/
-
-  private void updateNameAndDescription(Good result) {
-    if (result.getName() == null) {
-      result.setName("standard good");
-    }
-    if (result.getDescription() == null) {
-      result.setDescription("standard good description");
-    }
+    return DomainTestUtil.buildTestGood();
   }
 
   @Override
   protected void withoutPartOfUniqueConstraintEqualTest(Good standardEntity, String constraintName, String columnName) {
-
+    if (IdentifiedEntity.BASIC_CONSTRAINT_NAME.equals(constraintName)) {
+      withoutBasicConstraint(standardEntity, columnName);
+    }
   }
 
+  private void withoutBasicConstraint(Good standardEntity, String columnName) {
+    switch (columnName) {
+      case Good_.NAME: {
+        withoutColumnEqualTest(standardEntity, Good::getName, Good::setName);
+        return;
+      }
+      case Good_.PRODUCER: {
+        withoutColumnEqualTest(standardEntity, Good::getProducer, Good::setProducer);
+        return;
+      }
+      case Good_.CATEGORY: {
+        withoutColumnEqualTest(standardEntity, Good::getCategory, Good::setCategory);
+        return;
+      }
+      default: {
+        return;
+      }
+    }
+  }
+
+  @EqualsAndHashCode(callSuper = false)
   private static class MockGood extends Good {
 
     MockGood(Good good) {
+      super(buildCopyingGood(good));
+    }
+
+    private static Good buildCopyingGood(Good good) {
+      good.setProducer(null);
+      good.setCategory(null);
+      return good;
     }
   }
 }
