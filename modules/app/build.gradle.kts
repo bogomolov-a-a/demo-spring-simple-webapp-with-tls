b7390b9bf7e5c01d3d7d5df8ce3cfe82a940c2e0
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2021-01-03 00:22:00 +0300

    #154, spotbugs violations fixed!

--- modules/app/build.gradle.kts
+++ modules/app/build.gradle.kts
@@ -3,12 +3,18 @@ plugins {
     id("spring-boot-security-plugin")
     id("spring-boot-jpa-plugin")
     id("spring-boot-test-plugin")
-    id("io.freefair.lombok") version("5.3.0")
+    id("io.freefair.lombok") version ("5.3.0")
+    id("com.github.spotbugs") version ("4.6.0")
 }
 dependencies {
+    /*jdbc driver*/
     runtimeOnly("org.xerial:sqlite-jdbc")
+    /*spring dev tools*/
     runtimeOnly("org.springframework.boot:spring-boot-devtools")
-
+    /*use for any tests*/
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.apache.httpcomponents:httpclient")
+}
+spotbugs{
+    excludeFilter.set(File("config/spotbugs/excludes.xml"))
 }
--- modules/app/src/main/java/org/artembogomolova/demo/webapp/DemoWebappWithTlsApplication.java
+++ modules/app/src/main/java/org/artembogomolova/demo/webapp/DemoWebappWithTlsApplication.java
@@ -1,11 +1,9 @@
 package org.artembogomolova.demo.webapp;
 
-import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(exclude = {
     /*Exclude. Cause - conflicts with custom configuration*/
@@ -13,12 +11,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class DemoWebappWithTlsApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
-    SpringApplication application = new SpringApplication(DemoWebappWithTlsApplication.class);
-    /*for start context*/
-    application.setBannerMode(Mode.OFF);
-    try (ConfigurableApplicationContext context = application.run(args);) {
-      context.start();
-    }
+    SpringApplication.run(DemoWebappWithTlsApplication.class);
+
   }
 
 }
