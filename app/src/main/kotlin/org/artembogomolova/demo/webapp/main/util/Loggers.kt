bae43a65a2eef1b8a534ce8f7333488eaacd0c19
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2021-01-26 02:21:18 +0300

    #154, add logger function

---
+++ app/src/main/kotlin/org/artembogomolova/demo/webapp/main/util/Loggers.kt
@@ -0,0 +1,6 @@
+package org.artembogomolova.demo.webapp.main.util
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+fun getLogger(clazz: Class<*>): Logger = LoggerFactory.getLogger(clazz)
