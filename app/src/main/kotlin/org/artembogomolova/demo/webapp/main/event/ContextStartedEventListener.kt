90d7bf135254230ca04121a4eca14c38e5fc9758
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2021-01-26 02:21:03 +0300

    #154, updated from #144 branch.

--- app/src/main/kotlin/org/artembogomolova/demo/webapp/main/event/ContextStartedEventListener.kt
+++ app/src/main/kotlin/org/artembogomolova/demo/webapp/main/event/ContextStartedEventListener.kt
@@ -3,23 +3,29 @@ package org.artembogomolova.demo.webapp.main.event
 import org.artembogomolova.demo.webapp.main.dao.service.RoleRepoService
 import org.artembogomolova.demo.webapp.main.dao.service.UserRepoService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContextException
 import org.springframework.context.event.ContextStartedEvent
 import org.springframework.context.event.EventListener
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 
 @Component
-class ContextStartedEventListener{
+class ContextStartedEventListener {
     @Autowired
     lateinit var userRepoService: UserRepoService
+
     @Autowired
     lateinit var roleRepoService: RoleRepoService
+
     @Autowired
     lateinit var passwordEncoder: PasswordEncoder
 
     @EventListener
     fun onApplicationEvent(contextStartedEvent: ContextStartedEvent?) {
         println("application successful started")
+        if (userRepoService.corruptedDatabase()) {
+            throw ApplicationContextException("Database corrupted! Predefined admin and guest user deleted with unauthorized access!!!");
+        }
         if (!userRepoService.isFirstStart) {
             return
         }
