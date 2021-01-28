40c6a94ebbd509573bf46a9312c220ec048e05c2
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2021-01-29 02:31:33 +0300

    #154, add hibernate validation config with custom validation factory

---
+++ app/src/main/kotlin/org/artembogomolova/demo/webapp/main/config/CustomHibernateValidatorConfiguration.kt
@@ -0,0 +1,21 @@
+package org.artembogomolova.demo.webapp.main.config
+
+import org.hibernate.FlushMode
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Lazy
+import javax.validation.Validator
+
+@Configuration
+@Lazy
+class CustomHibernateValidatorConfiguration {
+    @Bean
+    @Lazy
+    fun hibernatePropertiesCustomizer(validator: Validator?): HibernatePropertiesCustomizer {
+        return HibernatePropertiesCustomizer { hibernateProperties: MutableMap<String?, Any?> ->
+            hibernateProperties["javax.persistence.validation.factory"] = validator
+            /*flush only on committed transaction.*/hibernateProperties["org.hibernate.flushMode"] = FlushMode.COMMIT.name
+        }
+    }
+}
