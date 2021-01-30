9c8a78f77baae044e65c0580f8ef98422ef82147
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2021-01-30 22:36:01 +0300

    #154, remove unused import

--- app/src/test/kotlin/org/artembogomolova/demo/webapp/test/db/dao/core/PersonDaoTest.kt
+++ app/src/test/kotlin/org/artembogomolova/demo/webapp/test/db/dao/core/PersonDaoTest.kt
@@ -9,7 +9,6 @@ import org.artembogomolova.demo.webapp.test.db.dao.DomainTestUtil
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.data.repository.CrudRepository
 
 @DisplayName("Dao test: Person")
